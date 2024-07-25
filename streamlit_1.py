efd6537f4e97af580c332431fde1da9a7b82e0fa
Anisa Ramadani <noezzy789@gmail.com>
2024-07-25 15:32:14 +0700

    Update streamlit_1.py

--- streamlit_1.py
+++ streamlit_1.py
@@ -4,37 +4,32 @@ import numpy as np
 import pyrebase
 import time
 import pickle
-from plyer import notification
-
-
-filename = 'XGBC_model.pkl'
 
 # Konfigurasi Firebase
 config = {
-  "apiKey": "AIzaSyDjHeDbeIotvRUuF0XZLeiRFjKgfv-6KHo",
-  "authDomain": "sensor-b15c7.firebaseapp.com",
-  "databaseURL": "https://sensor-b15c7-default-rtdb.firebaseio.com",
-  "projectId": "sensor-b15c7",
-  "storageBucket": "sensor-b15c7.appspot.com",
-  "messagingSenderId": "34611770004",
-  "appId": "1:34611770004:web:278e32c3bc729717ebba81"
+    "apiKey": "AIzaSyDjHeDbeIotvRUuF0XZLeiRFjKgfv-6KHo",
+    "authDomain": "sensor-b15c7.firebaseapp.com",
+    "databaseURL": "https://sensor-b15c7-default-rtdb.firebaseio.com",
+    "projectId": "sensor-b15c7",
+    "storageBucket": "sensor-b15c7.appspot.com",
+    "messagingSenderId": "34611770004",
+    "appId": "1:34611770004:web:278e32c3bc729717ebba81"
 }
 
 # Inisialisasi aplikasi Firebase
 firebase = pyrebase.initialize_app(config)
-
-# Akses database
 database = firebase.database()
 
-#load model
+# Load model
+filename = 'XGBC_model.pkl'
 with open(filename, 'rb') as file:
     loaded_model = pickle.load(file)
 
+st.title("HeninG! - Monitoring Kebisingan Kelas")
 
-st.title("HeninG!")
-
+# Pilihan Kelas
 opsi_kelas = ["XA", "XB", "XC", "XD"]
-kelas = st.selectbox("Kelas:", opsi_kelas)
+kelas = st.selectbox("Pilih Kelas:", opsi_kelas)
 
 sensor1, sensor2, sensor3 = st.columns(3)
 
@@ -45,9 +40,8 @@ if 'prev_max9814' not in st.session_state:
 if 'prev_soundSensor' not in st.session_state:
     st.session_state.prev_soundSensor = 0
 
+# Fungsi untuk memperbarui data
 def update_data():
-    global prev_max4466, prev_max9814, prev_soundSensor
-
     max4466 = database.child("/soundLevel%20MAX4466").get().val()
     max9814 = database.child("/soundLevel%20MAX9814").get().val()
     soundSensor = database.child("sound%20Sensor").get().val()
@@ -67,25 +61,20 @@ def update_data():
     with sensor3:
         st.metric(label="SoundLevel 3", value=f"{max9814:.2f} dB", delta=f"{delta_max9814:.2f} dB")
     
+    # Prediksi dengan model
     bising_test = [[soundSensor, max4466, max9814]]
     data_tes = np.array(bising_test)
     y_pred = loaded_model.predict(data_tes)
 
-    notification_title = "PERINGATAN"
-    notification_text = "Kelas sangat berisik"
-    notification_timeout = 1
     if y_pred == 1:
         st.error('Kelas berisik!!', icon="⚠️")
-    else :
-        st.write (''':green[Semua kelas Aman]''')
+        st.components.v1.html('<script>alert("Peringatan: Kelas sangat berisik!");</script>', height=0)
+    else:
+        st.success('Kelas dalam keadaan aman.', icon="✅")
 
-
-interval = 1
-
+interval = 5
 
 while True:
     update_data()
     time.sleep(interval)
     st.experimental_rerun()
-
-
